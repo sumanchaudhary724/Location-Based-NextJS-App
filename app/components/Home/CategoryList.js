4f8c02b4f6cbaf766b698a27a3248087022bbfc6
SumanChaudhary <sumanchaudhary724@gmail.com>
2024-01-13 01:14:59 +1100

    CategoryList Added

--- app/components/Home/CategoryList.js
+++ app/components/Home/CategoryList.js
@@ -1,9 +1,42 @@
 import React, { useState } from "react";
 import Data from "@/Shared/Data";
-
-const CategoryList = () => {
+import Image from "next/image";
+function CategoryList({ onCategoryChange }) {
   const [categoryList, setCategoryList] = useState(Data.CategoryListData);
-  return <div>CategoryList</div>;
-};
+  const [selectedCategory, setSelectedCategory] = useState();
+  return (
+    <div>
+      <h2 className="font-bold px-2">Select Food Type</h2>
+      <div
+        className="grid 
+        grid-cols-2 
+        md:grid-cols-2 
+        lg:grid-cols-3"
+      >
+        {categoryList.map((item, index) => (
+          <div
+            key={index}
+            className={`flex flex-col
+                justify-center items-center bg-gray-100
+                p-2 m-2 rounded-lg grayscale 
+                hover:grayscale-0 cursor-pointer
+                text-[13px]
+                 border-purple-400
+                ${
+                  selectedCategory == index ? "grayscale-0 border-[1px]" : null
+                }`}
+            onClick={() => {
+              setSelectedCategory(index);
+              onCategoryChange(item.value);
+            }}
+          >
+            <Image src={item.icon} alt={item.name} width={40} height={40} />
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default CategoryList;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import CategoryList from "./components/Home/CategoryList";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -11,5 +12,9 @@ export default function Home() {
       router.push("/login");
     }
   }, [session]);
-  return <div></div>;
+  return (
+    <div>
+      <CategoryList />
+    </div>
+  );
 }
